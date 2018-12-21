cc86e6417b2b128b6bd1ce795dafa35fe0a48dd2
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-12-21 22:49:06 +0000

    commit first promise

--- 01_promise.js
+++ 01_promise.js
@@ -1,13 +1,9 @@
 // Here is the first promise.
 
 function resolvePromise() {
-  return Promise.resolve("Success")
-    .then(function(value) {
-      console.log("Here is the promise: ", value);
-    })
-    .catch(function(value) {
-      console.log("There is an erro: ", value);
-    });
+  return Promise.resolve("Success").then(function(value) {
+    console.log("Here is the promise: ", value);
+  });
 }
 
 resolvePromise();
